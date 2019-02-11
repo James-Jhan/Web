a6afe8bb6e425c4fcf75131bb0e34da77b538279
James <james920x@gmail.com>
2019-02-11 14:20:00 +0800

    Adding jQuery example - class interactions & accessing content.

--- scripts/jQuery_html.js
+++ scripts/jQuery_html.js
@@ -1,3 +1,79 @@
+var index = 0;
+
 $(document).ready(function () {
-    $('p').text('Ready test function.');
+    change_inf();
+
+    $("#AddClass").click(function () {
+        $("#Flag").addClass("cont-opacity");
+    })
+
+    $("#RemoveClass").click(function () {
+        $("#Flag").removeClass("cont-opacity");
+    })
+
+    $("#ToggleClass").click(function () {
+        $("#Flag").toggleClass("cont-opacity");
+    })
+
+    $("#Next").click(function () {
+        index++;
+
+        if (index >= flag.length) {
+            index = 0;
+        }
+
+        $("#content").html(toHTML(index));
+    })
+
+    $("#content").html(toHTML(index));
 });
+
+var flag = new Array("../../images/America.png", "../../images/England.png", "../../images/Japan.png");
+var caption = new Array("America", "England", "Japan");
+var info = new Array("The United States of America (USA), commonly known as the United States (U.S. or US) or America, is a country composed of 50 states, a federal district, five major self-governing territories, and various possessions. At 3.8 million square miles (9.8 million km2), the United States is the world's third or fourth largest country by total area and is slightly smaller than the entire continent of Europe's 3.9 million square miles (10.1 million km2). With a population of over 327 million people, the U.S. is the third most populous country. The capital is Washington, D.C., and the largest city by population is New York. Forty-eight states and the capital's federal district are contiguous in North America between Canada and Mexico. The State of Alaska is in the northwest corner of North America, bordered by Canada to the east and across the Bering Strait from Russia to the west. The State of Hawaii is an archipelago in the mid-Pacific Ocean. The U.S. territories are scattered about the Pacific Ocean and the Caribbean Sea, stretching across nine official time zones. The extremely diverse geography, climate, and wildlife of the United States make it one of the world's 17 megadiverse countries.",
+ "England is a country that is part of the United Kingdom. It shares land borders with Wales to the west and Scotland to the north-northwest. The Irish Sea lies west of England and the Celtic Sea lies to the southwest. England is separated from continental Europe by the North Sea to the east and the English Channel to the south. The country covers five-eighths of the island of Great Britain, which lies in the North Atlantic, and includes over 100 smaller islands, such as the Isles of Scilly and the Isle of Wight.",
+  "Japan (Japanese: 日本; Nippon [ɲippoɴ] or Nihon [ɲihoɴ]; formally 日本国 About this soundNippon-koku or Nihon-koku, lit. State of Japan) is an island country in East Asia. Located in the Pacific Ocean, it lies off the eastern coast of the Asian continent and stretches from the Sea of Okhotsk in the north to the East China Sea and the Philippine Sea in the south.");
+
+var change_inf = function () {
+    var sel = $("#jQuery_Selector option:selected").index();
+
+    switch (sel) {
+        case 0:
+            $("#Selector_Name").text("Tag");
+            $("#Example").text("Selecting element by tag name.");
+            break;
+        case 1:
+            $("#Selector_Name").text("Class");
+            $("#Example").text("Selecting element by class name.");
+            break;
+        case 2:
+            $("#Selector_Name").text("ID");
+            $("#Example").text("Selecting element by property name.");
+            break;
+        case 3:
+            $("#Selector_Name").text("Property");
+            $("#Example").text("Selecting element by property name.");
+            break;
+        case 4:
+            $("#Selector_Name").text("Layer");
+            $("#Example").text("Selecting element by layer relationship.");
+            break;
+        case 5:
+            $("#Selector_Name").text("Group");
+            $("#Example").text("Selecting element by group relationship.");
+            break;
+        default:
+            alert("Error Selector");
+            break;
+    }
+};
+
+var toHTML = function () {
+    var fig = "<figure>";
+    fig += "<img src=" + "'" + flag[index] + "' " + "width='250' style='margin:10px' id='Flag'>";
+    fig += "<figcaption>" + caption[index] + "</figcaption>";
+    fig += "</figure>";
+    var hm = "<p>" + info[index] + "</p>";
+    var content = fig + hm;
+    return content;
+};
